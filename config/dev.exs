342909449ff22017b36ddc810ecea4d853bee5e7
Nylo Figueira Pinto <mastercoy@gmail.com>
2020-12-31 15:35:16 -0300

    feature: credo was added.

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :backend, Backend.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "root",
   database: "backend_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
