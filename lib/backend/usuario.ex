06bdee4d87669e4100337730f08698ee680c86d2
Nylo Figueira Pinto <mastercoy@gmail.com>
2021-01-03 17:42:49 -0300

    feature: schema migration was created

---
+++ lib/backend/usuario.ex
@@ -0,0 +1,25 @@
+defmodule Backend.Usuario do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :city, :string
+    field :complement_address, :string
+    field :cpf, :string
+    field :name, :string
+    field :neighborhood_address, :string
+    field :number_address, :string
+    field :postal_code, :string
+    field :state, :string
+    field :street_address, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(usuario, attrs) do
+    usuario
+    |> cast(attrs, [:name, :cpf, :street_address, :number_address, :complement_address, :neighborhood_address, :city, :state, :postal_code])
+    |> validate_required([:name, :cpf, :street_address, :number_address, :complement_address, :neighborhood_address, :city, :state, :postal_code])
+  end
+end
---
+++ priv/repo/migrations/20210103203635_create_users.exs
@@ -0,0 +1,20 @@
+defmodule Backend.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string, null: false
+      add :cpf, :string, null: false, unique: true
+      add :street_address, :string
+      add :number_address, :string
+      add :complement_address, :string
+      add :neighborhood_address, :string
+      add :city, :string
+      add :state, :string
+      add :postal_code, :string, null: false
+
+      timestamps()
+    end
+
+  end
+end
